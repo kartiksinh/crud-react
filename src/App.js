cef0a4e074cee06fe4f4a36fcf2bc4a6f12cfdf5
Kartiksinh <kartiksinh24@gmail.com>
2018-10-22 20:32:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  onClick(){
+    alert("hey ssup!!!");
+  }
+
   render() {
+     const List = [
+      'Item 1',
+      'Item 2',
+      'Item 3'
+     ]; 
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>
+          {
+            List.map(item=>{
+              return (
+                <div onClick={this.onClick}>{item}</div>
+              )
+            })
+          }
+        </h1>
       </div>
     );
   }
