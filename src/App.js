d59fac16689b6833cb706a66e7100fb1efdff2f5
Kartiksinh <kartiksinh24@gmail.com>
2018-10-23 17:54:35 +0530

    Add and delete functionality done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import MyComponent from './MyComponent';
+import ProductItem from './ProductItem';
+import AddProduct from './AddProduct';
 import './App.css';
 
 const products = [
@@ -19,25 +20,54 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      products: []
+      products: JSON.parse(localStorage.getItem('products'))
     };
+
+    this.onDelete = this.onDelete.bind(this);
+    this.onAdd = this.onAdd.bind(this);
   }
   componentWillMount(){
-    const products =  JSON.parse(localStorage.getItem('products'));
+    const products = this.getProducts();
     this.setState({ products });
+  }
+  getProducts(){
+   return this.state.products;
+  }
+
+  onAdd(name, price){
+    const products = this.getProducts();
+    products.push({
+      name,
+      price
+    });
+    this.setState({ products });
+  }
+
+  onDelete(name){
+    const products = this.getProducts();
+
+    const filteredProducts = products.filter( product=>{
+      return product.name !== name;
+    });
+    this.setState({products: filteredProducts });  
   }
 
   render() {
     return (
       <div className="App">
         <h1>Product Manager</h1>
-        {
+        <AddProduct
+          onAdd={this.onAdd} 
+        />
+        { 
           this.state.products.map(product =>{
             return(
-              <div key={product.name}>
-                <span>{product.name}</span> | <span>{product.price}</span>
-              </div>
-              )
+              <ProductItem
+                key={product.name}
+                {...product}
+                onDelete={this.onDelete}  
+              />
+            );
           })
         }
       </div>
